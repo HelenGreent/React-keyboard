fe246bfabda4719c2ee137b04a4fe60b30d71a0a
Helen <helengreent23@gmail.com>
2021-11-22 16:43:41 +0300

    keyboard_ui

---
+++ src/App.js
@@ -0,0 +1,216 @@
+import './App.css';
+
+function App() {
+  return (
+    <div classNameName="App">
+      <div className="cube-wrapper">
+			<div className="cube">
+				<div className="front-side">
+					<div className="container-I">
+						<div className="box">
+							<div className="container">
+								<div className="partition">
+									<div className="line-1">
+										<span className="esc f-key">
+											<span className="">esc</span>
+										</span>
+										<span className="f-key">f1</span>
+										<span className="f-key">f2</span>
+										<span className="f-key">f3</span>
+										<span className="f-key">f4</span>
+										<span className="f-key">f5</span>
+										<span className="f-key">f6</span>
+										<span className="f-key">f7</span>
+										<span className="f-key">f8</span>
+										<span className="f-key">f9</span>
+										<span className="f-key">f10</span>
+										<span className="f-key">f11</span>
+										<span className="f-key">f12</span>
+										<span className="touch-id f-key"></span>
+									</div>
+									<div className="line-2">
+										<div className="square">
+											<span className="content">~</span>
+											<span className="content">`</span>
+										</div>
+										<div className="square">
+											<span className="content">!</span>
+											<span className="content">1</span>
+										</div>
+										<div className="square">
+											<span className="content">@</span>
+											<span className="content">2</span>
+										</div>
+										<div className="square">
+											<span className="content">#</span>
+											<span className="content">3</span>
+										</div>
+										<div className="square">
+											<span className="content">$</span>
+											<span className="content">4</span>
+										</div>
+										<div className="square">
+											<span className="content">%</span>
+											<span className="content">5</span>
+										</div>
+										<div className="square">
+											<span className="content">^</span>
+											<span className="content">6</span>
+										</div>
+										<div className="square">
+											<span className="content">&</span>
+											<span className="content">7</span>
+										</div>
+										<div className="square">
+											<span className="content">*</span>
+											<span className="content">8</span>
+										</div>
+										<div className="square">
+											<span className="content">(</span>
+											<span className="content">9</span>
+										</div>
+										<div className="square">
+											<span className="content">)</span>
+											<span className="content">0</span>
+										</div>
+										<div className="square">
+											<span className="content">⎯</span>
+											<span className="content">-</span>
+										</div>
+										<div className="square">
+											<span className="content">+</span>
+											<span className="content">=</span>
+										</div>
+										<div className="square delete">
+											<span>delete</span>
+										</div>
+									</div>
+									<div className="line-3">
+										<div className="tab square"><span>tab</span></div>
+										<span className="square">Q</span>
+										<span className="square">W</span>
+										<span className="square">E</span>
+										<span className="square">R</span>
+										<span className="square">T</span>
+										<span className="square">Y</span>
+										<span className="square">U</span>
+										<span className="square">I</span>
+										<span className="square">O</span>
+										<span className="square">P</span>
+										<div className="square">
+											<span className="content">&#123;</span>
+                     					    <span className="content">[</span>
+										</div>
+										<div className="square">
+											<span className="content">&#125;</span>
+                     					    <span className="content">]</span>
+										</div>
+										<div className="square">
+											<span className="content">|</span
+											><span className="content">\</span>
+										</div>
+									</div>
+									<div className="line-4">
+										<div className="caps square">
+											<span className="">●</span><span className="">caps lock</span>
+										</div>
+										<span className="square">A</span>
+										<span className="square">S</span>
+										<span className="square">D</span>
+										<span className="square">F</span>
+										<span className="square">G</span>
+										<span className="square">H</span>
+										<span className="square">J</span>
+										<span className="square">K</span>
+										<span className="square">L</span>
+										<div className="square">
+											<span className="content">:</span
+											><span className="content">;</span>
+										</div>
+										<div className="square">
+											<span className="content">"</span
+											><span className="content">'</span>
+										</div>
+										<div className="delete square">
+											<span className="">return</span>
+										</div>
+									</div>
+									<div className="line-5">
+										<div className="shift square">
+											<span className="">shift</span>
+										</div>
+										<span className="square">Z</span>
+										<span className="square">X</span>
+										<span className="square">C</span>
+										<span className="square">V</span>
+										<span className="square">B</span>
+										<span className="square">N</span>
+										<span className="square">M</span>
+										<div className="square">
+											<span className="content"> {'<'} </span>
+                   						    <span className="content">,</span>
+										</div>
+										<div className="square">
+											<span className="content"> {'>'} </span>
+                    					    <span className="content">.</span>
+										</div>
+										<div className="square">
+											<span className="content">?</span
+											><span className="content">/</span>
+										</div>
+										<div className="shift2 square">
+											<span className="">shift</span>
+										</div>
+									</div>
+									<div className="line-2">
+										<div className="square font-size">
+											<div className="content fn">fn</div>
+											<span className="content sub-fn">⎈</span>
+										</div>
+										<div className="square font-size">
+											<span className="content fn">^</span>
+											<span className="content">control</span>
+										</div>
+										<div className="square font-size">
+											<span className="content fn">⎇</span>
+											<span className="content">option</span>
+										</div>
+										<div className="square font-size command">
+											<span className="content fn">⌘</span>
+											<span className="content">command</span>
+										</div>
+										<div className="square space-bar">
+											<span className="content"></span>
+										</div>
+										<div className="square font-size command">
+											<span className="content fn-last">⌘</span>
+											<span className="content">command</span>
+										</div>
+										<div className="square font-size">
+											<span className="content fn-last">⎇</span>
+											<span className="content">option</span>
+										</div>
+										<div className="arrow-wrapper">
+											<span className="f-key arrow">▲</span>
+											<div className="down-arrows">
+												<span className="f-key arrow">◀︎</span>
+												<span className="f-key arrow">▼</span>
+												<span className="f-key arrow">▶︎</span>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="track-pad"></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="back-side">
+				</div>
+			</div>
+		</div>
+    </div>
+  );
+}
+
+export default App;
